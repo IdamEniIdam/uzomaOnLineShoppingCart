fe868a2a72fb5c38ff676ce60f68dec076abbdbe
IdamEniIdam <idameni89@gmail.com>
2020-03-25 18:14:31 -0400

    Some CSS modificatio

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+ const Navbar = ()=>{
+    return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link style={{marginLeft: '-100px'}} to="/" className="brand-logo">Uzoma</Link>
+                    <ul className="right">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart">My cart</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                    </ul>
+                </div>
+            </nav>
+   
+        
+    )
+}
+
+export default Navbar;
